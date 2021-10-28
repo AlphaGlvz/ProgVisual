82f12219ba0073f01532b06034fb6f4bd2eb538e
LuisGalvez <71837539+AlphaGlvz@users.noreply.github.com>
2021-10-27 19:52:00 -0500

    Add files via upload

---
+++ Ejer01Get_Set/Ejer01Get_Set/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejer01Get_Set
+{
+
+    class Persona{
+        string nombre;
+        int edad;
+
+        //Constructor
+        public Persona(){}
+
+        public void SetNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public void SetEdad(int edad)
+        {
+            this.edad = edad;
+        }
+
+        public string GetNombre()
+        {
+            return nombre;
+        }
+
+        public int GetEdad()
+        {
+            return edad;
+        }
+    }
+
+    class Program{
+
+        static void Main(string[] args)
+        {
+            Persona p1 = new Persona();
+            p1.SetNombre("Luis Angel");
+            p1.SetEdad(24);
+
+            Console.WriteLine("Tu nombre es: " +p1.GetNombre());
+            Console.WriteLine("Tu edad es: " + p1.GetEdad());
+            Console.ReadLine();//Presionar Enter
+        }
+    }
+}
